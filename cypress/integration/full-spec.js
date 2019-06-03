056a0ceb19c8c85b8fa405280726a33ac712bcd7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-03 15:03:42 -0400

    remove persistence test because not implemented

--- cypress/integration/full-spec.js
+++ cypress/integration/full-spec.js
@@ -501,31 +501,6 @@ describe('TodoMVC - React', function () {
     })
   })
 
-  context('Persistence', function () {
-    it('should persist its data', function () {
-      // mimicking TodoMVC tests
-      // by writing out this function
-      function testState () {
-        cy.get('@firstTodo')
-          .should('contain', TODO_ITEM_ONE)
-          .and('have.class', 'completed')
-        cy.get('@secondTodo')
-          .should('contain', TODO_ITEM_TWO)
-          .and('not.have.class', 'completed')
-      }
-
-      cy.createTodo(TODO_ITEM_ONE).as('firstTodo')
-      cy.createTodo(TODO_ITEM_TWO).as('secondTodo')
-      cy.get('@firstTodo')
-        .find('.toggle')
-        .check()
-        .then(testState)
-
-        .reload()
-        .then(testState)
-    })
-  })
-
   context('Routing', function () {
     // New commands used here:
     // https://on.cypress.io/window
