fdabc746b387cb80f3e3461f222a61498b002c5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-03 22:17:15 -0400

    set initial todos for faster tests

--- cypress/integration/full-spec.js
+++ cypress/integration/full-spec.js
@@ -20,12 +20,40 @@
 // https://github.com/tastejs/todomvc/blob/master/tests/test.js
 // ***********************************************
 
-describe('TodoMVC - React', function () {
-  // setup these constants to match what TodoMVC does
-  let TODO_ITEM_ONE = 'buy some cheese'
-  let TODO_ITEM_TWO = 'feed the cat'
-  let TODO_ITEM_THREE = 'book a doctors appointment'
+// setup these constants to match what TodoMVC does
+const TODO_ITEM_ONE = 'buy some cheese'
+const TODO_ITEM_TWO = 'feed the cat'
+const TODO_ITEM_THREE = 'book a doctors appointment'
+// if you need 3 todos created instantly on load
+const initialState = [
+  {
+    id: 0,
+    text: TODO_ITEM_ONE,
+    completed: false
+  },
+  {
+    id: 1,
+    text: TODO_ITEM_TWO,
+    completed: false
+  },
+  {
+    id: 2,
+    text: TODO_ITEM_THREE,
+    completed: false
+  }
+]
 
+const visitWithInitialTodos = () => {
+  cy.visit('/', {
+    onBeforeLoad (win) {
+      // @ts-ignore
+      win.initialState = initialState
+    }
+  })
+  cy.get('.todo').as('todos')
+}
+
+describe('TodoMVC - React', function () {
   beforeEach(function () {
     // By default Cypress will automatically
     // clear the Local Storage prior to each
@@ -175,84 +203,6 @@ describe('TodoMVC - React', function () {
     })
   })
 
-  context('Mark all as completed', function () {
-    // New commands used here:
-    // - cy.check    https://on.cypress.io/api/check
-    // - cy.uncheck  https://on.cypress.io/api/uncheck
-
-    beforeEach(function () {
-      // This is an example of aliasing
-      // within a hook (beforeEach).
-      // Aliases will automatically persist
-      // between hooks and are available
-      // in your tests below
-      cy.createDefaultTodos().as('todos')
-    })
-
-    it('should allow me to mark all items as completed', function () {
-      // complete all todos
-      cy.get('[data-cy-toggle-all]').click()
-
-      // get each todo li and ensure its class is 'completed'
-      cy.get('@todos')
-        .eq(0)
-        .should('have.class', 'completed')
-      cy.get('@todos')
-        .eq(1)
-        .should('have.class', 'completed')
-      cy.get('@todos')
-        .eq(2)
-        .should('have.class', 'completed')
-    })
-
-    it('should allow me to clear the complete state of all items', function () {
-      // check and then immediately uncheck
-      cy.get('.toggle-all')
-        .check()
-        .uncheck()
-
-      cy.get('@todos')
-        .eq(0)
-        .should('not.have.class', 'completed')
-      cy.get('@todos')
-        .eq(1)
-        .should('not.have.class', 'completed')
-      cy.get('@todos')
-        .eq(2)
-        .should('not.have.class', 'completed')
-    })
-
-    it('complete all checkbox should update state when items are completed / cleared', function () {
-      cy.get('[data-cy-toggle-all]').click()
-
-      // alias the .toggle-all for reuse later
-      cy.get('.toggle-all')
-        .as('toggleAll')
-        // this assertion is silly here IMO but
-        // it is what TodoMVC does
-        .should('be.checked')
-
-      // alias the first todo and then click it
-      cy.get('.todo-list li')
-        .eq(0)
-        .as('firstTodo')
-        .find('.toggle')
-        .uncheck()
-
-      // reference the .toggle-all element again
-      // and make sure its not checked
-      cy.get('@toggleAll').should('not.be.checked')
-
-      // reference the first todo again and now toggle it
-      cy.get('@firstTodo')
-        .find('.toggle')
-        .check()
-
-      // assert the toggle all is checked again
-      cy.get('@toggleAll').should('be.checked')
-    })
-  })
-
   context('Item', function () {
     // New commands used here:
     // - cy.clear    https://on.cypress.io/api/clear
@@ -342,116 +292,6 @@ describe('TodoMVC - React', function () {
     })
   })
 
-  context('Editing', function () {
-    // New commands used here:
-    // - cy.blur    https://on.cypress.io/api/blur
-
-    beforeEach(function () {
-      cy.createDefaultTodos().as('todos')
-    })
-
-    it('should hide other controls when editing', function () {
-      cy.get('@todos')
-        .eq(1)
-        .as('secondTodo')
-        .find('label')
-        .dblclick()
-
-      cy.get('@secondTodo')
-        .find('.toggle')
-        .should('not.be.visible')
-      cy.get('@secondTodo')
-        .find('label')
-        .should('not.be.visible')
-    })
-
-    it('should save edits on blur', function () {
-      cy.get('@todos')
-        .eq(1)
-        .as('secondTodo')
-        .find('label')
-        .dblclick()
-
-      cy.get('@secondTodo')
-        .find('.edit')
-        .clear()
-        .type('buy some sausages')
-        // we can just send the blur event directly
-        // to the input instead of having to click
-        // on another button on the page. though you
-        // could do that its just more mental work
-        .blur()
-
-      cy.get('@todos')
-        .eq(0)
-        .should('contain', TODO_ITEM_ONE)
-      cy.get('@secondTodo').should('contain', 'buy some sausages')
-      cy.get('@todos')
-        .eq(2)
-        .should('contain', TODO_ITEM_THREE)
-    })
-
-    it('should trim entered text', function () {
-      cy.get('@todos')
-        .eq(1)
-        .as('secondTodo')
-        .find('label')
-        .dblclick()
-
-      cy.get('@secondTodo')
-        .find('.edit')
-        .clear()
-        .type('    buy some sausages    ')
-        .type('{enter}')
-
-      cy.get('@todos')
-        .eq(0)
-        .should('contain', TODO_ITEM_ONE)
-      cy.get('@secondTodo').should('contain', 'buy some sausages')
-      cy.get('@todos')
-        .eq(2)
-        .should('contain', TODO_ITEM_THREE)
-    })
-
-    it('should remove the item if an empty text string was entered', function () {
-      cy.get('@todos')
-        .eq(1)
-        .as('secondTodo')
-        .find('label')
-        .dblclick()
-
-      cy.get('@secondTodo')
-        .find('.edit')
-        .clear()
-        .type('{enter}')
-
-      cy.get('@todos').should('have.length', 2)
-    })
-
-    it.skip('should cancel edits on escape', function () {
-      cy.get('@todos')
-        .eq(1)
-        .as('secondTodo')
-        .find('label')
-        .dblclick()
-
-      cy.get('@secondTodo')
-        .find('.edit')
-        .clear()
-        .type('foo{esc}')
-
-      cy.get('@todos')
-        .eq(0)
-        .should('contain', TODO_ITEM_ONE)
-      cy.get('@todos')
-        .eq(1)
-        .should('contain', TODO_ITEM_TWO)
-      cy.get('@todos')
-        .eq(2)
-        .should('contain', TODO_ITEM_THREE)
-    })
-  })
-
   context('Counter', function () {
     it('should display the current number of todo items', function () {
       cy.createTodo(TODO_ITEM_ONE)
@@ -460,132 +300,278 @@ describe('TodoMVC - React', function () {
       cy.get('.todo-count').contains('2 items left')
     })
   })
+})
 
-  context('Clear completed button', function () {
-    beforeEach(function () {
-      cy.createDefaultTodos().as('todos')
-    })
+context('Mark all as completed', function () {
+  // New commands used here:
+  // - cy.check    https://on.cypress.io/api/check
+  // - cy.uncheck  https://on.cypress.io/api/uncheck
 
-    it('should display the correct text', function () {
-      cy.get('@todos')
-        .eq(0)
-        .find('.toggle')
-        .check()
-      cy.get('.clear-completed').contains('Clear completed')
-    })
+  beforeEach(visitWithInitialTodos)
 
-    it('should remove completed items when clicked', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.clear-completed').click()
-      cy.get('@todos').should('have.length', 2)
-      cy.get('@todos')
-        .eq(0)
-        .should('contain', TODO_ITEM_ONE)
-      cy.get('@todos')
-        .eq(1)
-        .should('contain', TODO_ITEM_THREE)
-    })
+  it('should allow me to mark all items as completed', function () {
+    // complete all todos
+    cy.get('[data-cy-toggle-all]').click()
 
-    it('should be hidden when there are no items that are completed', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.clear-completed')
-        .should('be.visible')
-        .click()
-      cy.get('.clear-completed').should('not.exist')
-    })
+    // get each todo li and ensure its class is 'completed'
+    cy.get('@todos')
+      .eq(0)
+      .should('have.class', 'completed')
+    cy.get('@todos')
+      .eq(1)
+      .should('have.class', 'completed')
+    cy.get('@todos')
+      .eq(2)
+      .should('have.class', 'completed')
   })
 
-  context('Routing', function () {
-    // New commands used here:
-    // https://on.cypress.io/window
-    // https://on.cypress.io/its
-    // https://on.cypress.io/invoke
-    // https://on.cypress.io/within
+  it('should allow me to clear the complete state of all items', function () {
+    // check and then immediately uncheck
+    cy.get('.toggle-all')
+      .check()
+      .uncheck()
 
-    beforeEach(function () {
-      cy.createDefaultTodos().as('todos')
-    })
+    cy.get('@todos')
+      .eq(0)
+      .should('not.have.class', 'completed')
+    cy.get('@todos')
+      .eq(1)
+      .should('not.have.class', 'completed')
+    cy.get('@todos')
+      .eq(2)
+      .should('not.have.class', 'completed')
+  })
 
-    it('should allow me to display active items', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.filters')
-        .contains('Active')
-        .click()
-      cy.get('@todos')
-        .eq(0)
-        .should('contain', TODO_ITEM_ONE)
-      cy.get('@todos')
-        .eq(1)
-        .should('contain', TODO_ITEM_THREE)
-    })
+  it('complete all checkbox should update state when items are completed / cleared', function () {
+    cy.get('[data-cy-toggle-all]').click()
 
-    it.skip('should respect the back button', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.filters')
-        .contains('Active')
-        .click()
-      cy.get('.filters')
-        .contains('Completed')
-        .click()
-      cy.get('@todos').should('have.length', 1)
-      cy.go('back')
-      cy.get('@todos').should('have.length', 2)
-      cy.go('back')
-      cy.get('@todos').should('have.length', 3)
-    })
+    // alias the .toggle-all for reuse later
+    cy.get('.toggle-all')
+      .as('toggleAll')
+      // this assertion is silly here IMO but
+      // it is what TodoMVC does
+      .should('be.checked')
 
-    it('should allow me to display completed items', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.filters')
-        .contains('Completed')
-        .click()
-      cy.get('@todos').should('have.length', 1)
-    })
+    // alias the first todo and then click it
+    cy.get('.todo-list li')
+      .eq(0)
+      .as('firstTodo')
+      .find('.toggle')
+      .uncheck()
 
-    it('should allow me to display all items', function () {
-      cy.get('@todos')
-        .eq(1)
-        .find('.toggle')
-        .check()
-      cy.get('.filters')
-        .contains('Active')
-        .click()
-      cy.get('.filters')
-        .contains('Completed')
-        .click()
-      cy.get('.filters')
-        .contains('All')
-        .click()
-      cy.get('@todos').should('have.length', 3)
-    })
+    // reference the .toggle-all element again
+    // and make sure its not checked
+    cy.get('@toggleAll').should('not.be.checked')
 
-    it('should highlight the currently applied filter', function () {
-      // using a within here which will automatically scope
-      // nested 'cy' queries to our parent element <ul.filters>
-      cy.get('.filters').within(function () {
-        cy.contains('All').should('have.class', 'selected')
-        cy.contains('Active')
-          .click()
-          .should('have.class', 'selected')
-        cy.contains('Completed')
-          .click()
-          .should('have.class', 'selected')
-      })
+    // reference the first todo again and now toggle it
+    cy.get('@firstTodo')
+      .find('.toggle')
+      .check()
+
+    // assert the toggle all is checked again
+    cy.get('@toggleAll').should('be.checked')
+  })
+})
+
+context('Editing', function () {
+  // New commands used here:
+  // - cy.blur    https://on.cypress.io/api/blur
+
+  beforeEach(visitWithInitialTodos)
+
+  it('should hide other controls when editing', function () {
+    cy.get('@todos')
+      .eq(1)
+      .as('secondTodo')
+      .find('label')
+      .dblclick()
+
+    cy.get('@secondTodo')
+      .find('.toggle')
+      .should('not.be.visible')
+    cy.get('@secondTodo')
+      .find('label')
+      .should('not.be.visible')
+  })
+
+  it('should save edits on blur', function () {
+    cy.get('@todos')
+      .eq(1)
+      .as('secondTodo')
+      .find('label')
+      .dblclick()
+
+    cy.get('@secondTodo')
+      .find('.edit')
+      .clear()
+      .type('buy some sausages')
+      // we can just send the blur event directly
+      // to the input instead of having to click
+      // on another button on the page. though you
+      // could do that its just more mental work
+      .blur()
+
+    cy.get('@todos')
+      .eq(0)
+      .should('contain', TODO_ITEM_ONE)
+    cy.get('@secondTodo').should('contain', 'buy some sausages')
+    cy.get('@todos')
+      .eq(2)
+      .should('contain', TODO_ITEM_THREE)
+  })
+
+  it('should trim entered text', function () {
+    cy.get('@todos')
+      .eq(1)
+      .as('secondTodo')
+      .find('label')
+      .dblclick()
+
+    cy.get('@secondTodo')
+      .find('.edit')
+      .clear()
+      .type('    buy some sausages    ')
+      .type('{enter}')
+
+    cy.get('@todos')
+      .eq(0)
+      .should('contain', TODO_ITEM_ONE)
+    cy.get('@secondTodo').should('contain', 'buy some sausages')
+    cy.get('@todos')
+      .eq(2)
+      .should('contain', TODO_ITEM_THREE)
+  })
+
+  it('should remove the item if an empty text string was entered', function () {
+    cy.get('@todos')
+      .eq(1)
+      .as('secondTodo')
+      .find('label')
+      .dblclick()
+
+    cy.get('@secondTodo')
+      .find('.edit')
+      .clear()
+      .type('{enter}')
+
+    cy.get('@todos').should('have.length', 2)
+  })
+})
+
+context('Clear completed button', function () {
+  beforeEach(visitWithInitialTodos)
+
+  it('should display the correct text', function () {
+    cy.get('@todos')
+      .eq(0)
+      .find('.toggle')
+      .check()
+    cy.get('.clear-completed').contains('Clear completed')
+  })
+
+  it('should remove completed items when clicked', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.clear-completed').click()
+    cy.get('@todos').should('have.length', 2)
+    cy.get('@todos')
+      .eq(0)
+      .should('contain', TODO_ITEM_ONE)
+    cy.get('@todos')
+      .eq(1)
+      .should('contain', TODO_ITEM_THREE)
+  })
+
+  it('should be hidden when there are no items that are completed', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.clear-completed')
+      .should('be.visible')
+      .click()
+    cy.get('.clear-completed').should('not.exist')
+  })
+})
+
+context('Routing', function () {
+  beforeEach(visitWithInitialTodos)
+
+  it('should allow me to display active items', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.filters')
+      .contains('Active')
+      .click()
+    cy.get('@todos')
+      .eq(0)
+      .should('contain', TODO_ITEM_ONE)
+    cy.get('@todos')
+      .eq(1)
+      .should('contain', TODO_ITEM_THREE)
+  })
+
+  it.skip('should respect the back button', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.filters')
+      .contains('Active')
+      .click()
+    cy.get('.filters')
+      .contains('Completed')
+      .click()
+    cy.get('@todos').should('have.length', 1)
+    cy.go('back')
+    cy.get('@todos').should('have.length', 2)
+    cy.go('back')
+    cy.get('@todos').should('have.length', 3)
+  })
+
+  it('should allow me to display completed items', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.filters')
+      .contains('Completed')
+      .click()
+    cy.get('@todos').should('have.length', 1)
+  })
+
+  it('should allow me to display all items', function () {
+    cy.get('@todos')
+      .eq(1)
+      .find('.toggle')
+      .check()
+    cy.get('.filters')
+      .contains('Active')
+      .click()
+    cy.get('.filters')
+      .contains('Completed')
+      .click()
+    cy.get('.filters')
+      .contains('All')
+      .click()
+    cy.get('@todos').should('have.length', 3)
+  })
+
+  it('should highlight the currently applied filter', function () {
+    // using a within here which will automatically scope
+    // nested 'cy' queries to our parent element <ul.filters>
+    cy.get('.filters').within(function () {
+      cy.contains('All').should('have.class', 'selected')
+      cy.contains('Active')
+        .click()
+        .should('have.class', 'selected')
+      cy.contains('Completed')
+        .click()
+        .should('have.class', 'selected')
     })
   })
 })
