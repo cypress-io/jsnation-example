b4ec283c252bd8f8c83da9dfe145f71bc78c3886
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-05 13:57:37 -0400

    add some comments

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,9 @@
 module.exports = (on, config) => {
   on('task', require('@cypress/code-coverage/task'))
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-
+  // tell Cypress to use .babelrc file
+  // and instrument the specs files
+  // only the extra application files will be instrumented
+  // not the spec files themselves
   return config
 }
